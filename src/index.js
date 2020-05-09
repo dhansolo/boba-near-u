5cbc237c5caa756294b0add6630a0818883055d7
David Han <dhansolo@uw.edu>
2020-05-09 00:39:26 -0700

    Ran into trouble when trying to setState without user interaction so i had to switch up the overall design; added intro text and button that makes http call to Yelp; added logic to alter some DOM elements as well as alter what is shown on screen

--- src/index.js
+++ src/index.js
@@ -2,28 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
-class GetBoba extends React.Component {
+let lat;
+let long;
+let intro;
+let button;
+let results;
+
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lat: "",
-            long: "",
             data: null,
-            nearestBobaName: "",
-            nearestBobaAddress: "",
-            nearestBobaCity: "",
-            nearestBobaZip: "",
-            nearestBobaState: "",
-            nearestBobaPhone: "",
-            nearestBobaDistance: ""
+            displayIntro: true,
+            displayResults: false
         }
+        this.handleBobaClick = this.handleBobaClick.bind(this);
     }
 
     componentDidMount() {
-        if("geolocation" in navigator) {
+        if(navigator.geolocation) {
+            console.log("getting current location")
             navigator.geolocation.getCurrentPosition(
             function(position) {
-                this.setState({ lat: position.coords.latitude, long: position.coords.longitude});
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+                console.log("location acquired");
+                let el = document.createElement('p');
+                el.innerText = "Current Location: " + lat.toFixed(3) + ", " + long.toFixed(3);
+                document.getElementById('current-location').appendChild(el);
             },
             function(error) {
                 console.log("Please allow us to stalk you");
@@ -33,67 +39,112 @@ class GetBoba extends React.Component {
         }
     }
 
-    getBoba(lat, long) {
-        return axios({
-            "method":"GET",
-            "url":"https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search",
-            "headers":{
-                "Access-Control-Allow-Origin":"*",
-                "Authorization": `Bearer ${process.env.REACT_APP_YELP_KEY}`,
+    handleBobaClick() {
+        if(navigator.geolocation) {
+            console.log("getting current location")
+            navigator.geolocation.getCurrentPosition(
+            function(position) {
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+                console.log("location acquired");
+                let current = document.getElementById('current-location');
+                while(current.firstChild) {
+                    current.removeChild(current.lastChild);
+                }
+                let el = document.createElement('p');
+                el.innerText = "Current Location: " + lat.toFixed(3) + ", " + long.toFixed(3);
+                document.getElementById('current-location').appendChild(el);
             },
-            "params":{
-                "term":process.env.REACT_APP_SEARCH_TERM,
-                "latitude": lat,
-                "longitude": long,
-                "sort_by":"distance",
-                "limit": 50,
-                "radius": 40000
-            }
-        })
-        .then((response)=> {
-            console.log(response);
-            if(response.data.businesses > 0) {
-                this.setState({
-                    data: response.data.businesses,
-                    nearestBobaName: response.data.businesses[0].name,
-                    nearestBobaAddress: response.data.businesses[0].location.address1,
-                    nearestBobaCity: response.data.businesses[0].location.city,
-                    nearestBobaZip: response.data.businesses[0].location.zip_code,
-                    nearestBobaState: response.data.businesses[0].location.state,
-                    nearestBobaPhone: response.data.businesses[0].display_phone,
-                    nearestBobaDistance: (response.data.businesses[0].distance * 0.000621371)
-                })
-            } else {
-                console.log("DAMN BRUH AINT NO BOBA AROUND YOU FOR 25 MILES")
-            }
-        })
-        .catch((error)=> {
-            console.log(error);
-        })
+            function(error) {
+                console.log("Please allow us to stalk you");
+            });
+        }
+        if(lat && long) {
+            intro = null;
+            results = null;
+            document.getElementById('boba-button').innerText = "Refresh";
+            this.setState({ displayIntro: false, displayResults: false });
+            axios({
+                "method":"GET",
+                "url":"https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search",
+                "headers":{
+                    "Access-Control-Allow-Origin":"*",
+                    "Authorization": `Bearer ${process.env.REACT_APP_YELP_KEY}`,
+                },
+                "params":{
+                    "term":process.env.REACT_APP_SEARCH_TERM,
+                    "latitude": lat,
+                    "longitude": long,
+                    "sort_by":"distance",
+                    "limit": 50,
+                    "radius": 40000
+                }
+            })
+            .then((response)=> {
+                if(response.data.businesses.length > 0) {
+                    this.setState({
+                        data: response.data.businesses,
+                        displayResults: true,
+                    })
+                } else {
+                    console.log("DAMN BRUH AINT NO BOBA AROUND YOU FOR 25 MILES")
+                }
+            })
+            .catch((error)=> {
+                console.log(error);
+                return;
+            })
+        }
     }
 
     render() {
+        if(this.state.displayIntro) { intro = <IntroPage /> }
+        if(this.state.displayResults) {  results = <DisplayNearest data={this.state.data}/>}
         return (
-            <ul>
-                <li>{this.state.nearestBobaName}</li>
-                <li>{this.state.nearestBobaAddress}</li>
-                <li>{this.state.nearestBobaCity}</li>
-                <li>{this.state.nearestBobaZip}</li>
-                <li>{this.state.nearestBobaState}</li>
-                <li>{this.state.nearestBobaPhone}</li>
-                <li>{this.state.nearestBobaDistance}</li>
-            </ul>
+            <div>
+                <h1>Boba Near U</h1>
+                <div id="current-location"></div>
+                {intro}
+                <button id="boba-button" onClick={this.handleBobaClick}>Find Boba!</button>
+                {results}
+            </div>
         )
     }
 }
 
-function App() {
-    return(
-        <div>
-            <h1>Boba Near U</h1>
-            <GetBoba />
-        </div>
-    );
+class IntroPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is an application that takes your current location, wherever you are in the world, and finds the nearest boba/bubbletea parlor relative to your locaton</p>
+                <p>If location permissions have not been enabled, you should have been greeted by an alert asking for permission</p>
+                <p>Please be sure to accept location permissions and click on the button below to begin</p>
+            </div>
+        )
+    }
+}
+
+class DisplayNearest extends React.Component {
+    render() {
+        let distance = (this.props.data[0].distance * 0.000621371)
+        return (
+            <div>
+                <h1>{this.props.data[0].name}</h1>
+                <p>{this.props.data[0].location.address1}</p>
+                <p>{this.props.data[0].location.city}, {this.props.data[0].location.state} {this.props.data[0].location.zip_code}</p>
+                <p>{this.props.data[0].display_phone}</p>
+                {<p>{distance.toFixed(3)} miles away</p>}
+            </div>
+        )
+    }
+}
+
+class LoadAnimation extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
