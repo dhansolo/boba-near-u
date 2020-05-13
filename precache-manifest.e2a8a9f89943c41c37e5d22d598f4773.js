c9cbe2408a3ebea2461a8604db344ce5c949de38
David Han <dhansolo@uw.edu>
2020-05-13 16:21:23 -0700

    Updates

---
+++ precache-manifest.e2a8a9f89943c41c37e5d22d598f4773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a9d3eb6481687e77c90d550f4d32c4",
+    "url": "/boba-near-u/index.html"
+  },
+  {
+    "revision": "d1b2828053875f688ed0",
+    "url": "/boba-near-u/static/css/main.c4cbd921.chunk.css"
+  },
+  {
+    "revision": "5e2334e1ebb1314c893c",
+    "url": "/boba-near-u/static/js/2.80dc0925.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/boba-near-u/static/js/2.80dc0925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b2828053875f688ed0",
+    "url": "/boba-near-u/static/js/main.02dcebc3.chunk.js"
+  },
+  {
+    "revision": "0b17fa534a274b0af3e1",
+    "url": "/boba-near-u/static/js/runtime-main.3464148e.js"
+  },
+  {
+    "revision": "80c77471c3ce0e0adb84028877ea409f",
+    "url": "/boba-near-u/static/media/boba.80c77471.png"
+  }
+]);
